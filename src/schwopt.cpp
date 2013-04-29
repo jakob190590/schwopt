30a003e2ba99ae28d8c058a94901780b8558229e
Jakob Schöttl <jschoett@gmail.com>
2013-04-29 19:15:02 +0200

    Ausgabe verschoenert

--- src/schwopt.cpp
+++ src/schwopt.cpp
@@ -13,7 +13,6 @@
 
 #include "Schwimmer.h"
 #include "Zeit.h"
-#include "computer/OptComputer.h"
 #include "computer/LagenstaffelComputer1.h"
 #include "computer/LagenstaffelComputer2.h"
 #include "computer/KraulstaffelComputer.h"
@@ -38,23 +37,28 @@ int main(int argc, char* argv[]) {
 	{
 		Schwimmer* schw = new Schwimmer();
 		ifs >> schw;
-		cout << schw;
+//		cout << schw;
 		schwimmer.push_back(schw);
 	}
 	ifs.close();
 
 	// So, ab hier kann mit dem vector schwimmer gearbeitet werden
+	cout << endl << "Nachname       Vorname Kurzl brust50 brust100 rueck50 rueck100 schm50 schm100 frei50 frei100";
+	cout << endl << "--------------------------------------------------------------------------------------------" << endl;
 	cout << schwimmer << endl;
 
 //	OptComputer optComputer(schwimmer);
+	cout << "// LagenstaffelComputer1 (Algo)" << endl;
 	LagenstaffelComputer1 lagenstaffelComputer1(schwimmer);
 	lagenstaffelComputer1.compute();
 	lagenstaffelComputer1.outputResult(cout);
 
+	cout << "// LagenstaffelComputer2 (Durchprobieren, optimal)" << endl;
 	LagenstaffelComputer2 lagenstaffelComputer2(schwimmer);
 	lagenstaffelComputer2.compute();
 	lagenstaffelComputer2.outputResult(cout);
 
+	cout << "// KraulstaffelComputer (optimal)" << endl;
 	KraulstaffelComputer kraulstaffelComputer(schwimmer);
 	kraulstaffelComputer.compute();
 	kraulstaffelComputer.outputResult(cout);
