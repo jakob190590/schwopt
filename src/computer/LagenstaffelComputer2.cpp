5424646115c53281a2c08278e024877489d4b8f6
Jakob Schöttl <jschoett@gmail.com>
2013-05-01 22:24:02 +0200

    Mixed-Bedingung fuer exakte Loesung

--- src/computer/LagenstaffelComputer2.cpp
+++ src/computer/LagenstaffelComputer2.cpp
@@ -39,6 +39,8 @@ LagenstaffelComputer2::LagenstaffelComputer2(const SchwimmerVector& schwimmer) :
  */
 void LagenstaffelComputer2::compute()
 {
+	typedef vector<int> IntVec;
+
 	result.resize(ANZAHL_POSITIONEN_IN_STAFFEL);
 
 	typedef SchwimmerVector::const_iterator It;
@@ -47,8 +49,14 @@ void LagenstaffelComputer2::compute()
 
 	unsigned best = UINT_MAX;
 
+	// "Mixed"-Bedingungen: 2 Schwimmer, 2 Schwimmerinnen
+	IntVec neededGeschlecht(2, 2); // 2 elemente, beide werte sind 2
+
 	for(It it_s0 = begin; it_s0 != end; ++it_s0)
 	{
+		IntVec needed0(neededGeschlecht);
+		needed0[(*it_s0)->geschlecht]--;
+
 		unsigned zeit0 = (*it_s0)->zeiten[Disziplin::RUECK_50];
 		if (zeit0 == Zeit::MAX_UNSIGNED_VALUE)
 			continue;
@@ -58,6 +66,10 @@ void LagenstaffelComputer2::compute()
 		{
 			if (it_s1 == it_s0)
 				continue;
+
+			IntVec needed1(needed0);
+			needed1[(*it_s1)->geschlecht]--;
+			// Beim 2. Schwimmer noch keine Pruefung noetig
 
 			unsigned zeit1 = (*it_s1)->zeiten[Disziplin::BRUST_50];
 			if (zeit1 == Zeit::MAX_UNSIGNED_VALUE)
@@ -70,6 +82,12 @@ void LagenstaffelComputer2::compute()
 				if (it_s2 == it_s1 || it_s2 == it_s0)
 					continue;
 
+				IntVec needed2(needed1);
+				if (needed2[(*it_s2)->geschlecht] > 0)
+					needed2[(*it_s2)->geschlecht]--;
+				else
+					continue;
+
 				unsigned zeit2 = (*it_s2)->zeiten[Disziplin::SCHM_50];
 				if (zeit2 == Zeit::MAX_UNSIGNED_VALUE)
 					continue;
@@ -79,6 +97,12 @@ void LagenstaffelComputer2::compute()
 				for (It it_s3 = begin; it_s3 != end; ++it_s3)
 				{
 					if (it_s3 == it_s2 || it_s3 == it_s1 || it_s3 == it_s0)
+						continue;
+
+					IntVec needed3(needed2);
+					if (needed3[(*it_s3)->geschlecht] > 0)
+						needed3[(*it_s3)->geschlecht]--;
+					else
 						continue;
 
 					unsigned zeit3 = (*it_s3)->zeiten[Disziplin::FREI_50];
