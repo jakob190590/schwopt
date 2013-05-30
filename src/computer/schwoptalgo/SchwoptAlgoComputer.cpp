defe32c63437370e4917a6f7aedcc0957bb84701
Jakob Schöttl <jschoett@gmail.com>
2013-05-30 13:20:23 +0200

    Debugausgabe verbessert

--- src/computer/schwoptalgo/SchwoptAlgoComputer.cpp
+++ src/computer/schwoptalgo/SchwoptAlgoComputer.cpp
@@ -102,7 +102,7 @@ SchwoptAlgoComputer::SchwoptAlgoComputer(const SchwimmerVector& schwimmer) :
 
 ostream& SchwoptAlgoComputer::outputResult(ostream& os)
 {
-	for (int pos = 0; pos < disziplinenAufPositionen.size(); pos++)
+	for (unsigned pos = 0; pos < disziplinenAufPositionen.size(); pos++)
 	{
 		int diszi = disziplinenAufPositionen[pos];
 		os << setiosflags(ios::left) << setw(15) << Disziplin::convertToString(diszi, true, false);
@@ -160,16 +160,24 @@ void SchwoptAlgoComputer::gscheideDebugAusgabe(ostream& os,
 		const SchwoptAlgoComputer::SchwimmerAbstandMap abstaende[Disziplin::ANZAHL],
 		unsigned anzahlNaechstbester, bool showDisziplin) const
 {
+	const int COL_WIDTH_POSITION = 5;
+	const int COL_WIDTH_DISZIPLIN = 10;
+	const int COL_WIDTH_SCHWIMMER = 6;
+	const int COL_WIDTH_ZEIT = 14;
+
+	os << setiosflags(ios::left); // default
 
 	// Ueberschriftenzeile
-	os << "Pos. ";
+	os     << setw(COL_WIDTH_POSITION)  << "Pos.";
 	if (showDisziplin)
-		os << "Disziplin ";
-	os << "Schw. Zeit       ";
-//	for (unsigned i = 2; i < anzahlNaechstbester + 2; i++)
+		os << setw(COL_WIDTH_DISZIPLIN) << "Disziplin";
+	os     << setw(COL_WIDTH_SCHWIMMER) << "Schw.";
+	os     << setw(COL_WIDTH_ZEIT)      << "Zeit";
+
+//	for (unsigned i = 2; i < anzahlNaechstbester + 2; i++) // so detailiert muessen die header nicht sein
 //		os << i << ". Schwimmer ";
 	if (anzahlNaechstbester > 0)
-		os << "Naechstbessere Schwimmer";
+		os << "Naechstbeste Schwimmer";
 	os << endl;
 
 	// Daten
@@ -179,11 +187,12 @@ void SchwoptAlgoComputer::gscheideDebugAusgabe(ostream& os,
 		const int disziplin = disziplinen[position];
 		Schwimmer* const schwimmer = vec[i].second;
 
-		os << position;
+		os     << setw(COL_WIDTH_POSITION)  << position;
 		if (showDisziplin)
-			os << Disziplin::convertToString(disziplin, true, true, "m");
-		os << schwimmer->kuerzel << schwimmer->zeiten[disziplin];
-		os << "-" << abstaende[disziplin].find(schwimmer)->second << "-";
+			os << setw(COL_WIDTH_DISZIPLIN) << Disziplin::convertToString(disziplin, true, true, "m").substr(0, COL_WIDTH_DISZIPLIN - 2);
+		os     << setw(COL_WIDTH_SCHWIMMER) << schwimmer->kuerzel;
+		os     << setw(COL_WIDTH_ZEIT / 2 - 1) << schwimmer->zeiten[disziplin];
+		os     << "-" << setw(4) << setiosflags(ios::right) << abstaende[disziplin].find(schwimmer)->second << resetiosflags(ios::right) << "-  ";
 
 		const SchwimmerList& naechstbesteSchwimmer = schwimmerSortiert[disziplin];
 		SchwimmerList::const_iterator next = naechstbesteSchwimmer.begin();
@@ -191,8 +200,8 @@ void SchwoptAlgoComputer::gscheideDebugAusgabe(ostream& os,
 		for (unsigned i = 0; i < anzahlNaechstbester && next != naechstbesteSchwimmer.end(); i++)
 		{
 			Schwimmer* const nextSchwimmer = *next;
-			os << nextSchwimmer->zeiten[disziplin] << " (" << nextSchwimmer->kuerzel << ") ";
-			os << "-" << abstaende[disziplin].find(nextSchwimmer)->second << "-";
+			os << nextSchwimmer->kuerzel << ' ' << nextSchwimmer->zeiten[disziplin];
+			os << " -" << abstaende[disziplin].find(nextSchwimmer)->second << "- ";
 		}
 		os << endl;
 	}
